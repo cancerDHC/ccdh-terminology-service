421b92393683a37110bd382a4398bca4a1cf8ceb
jiaola <djiao@jhu.edu>
2020-08-13 11:59:21 -0400

    pdc test

---
+++ tests/test_pdc.py
@@ -0,0 +1,12 @@
+import pytest
+from ccdh.pdc import PDCDictionary, PDC_ROOT
+
+
+@pytest.fixture
+def pdc_dictionary():
+    return PDCDictionary(root_dir=PDC_ROOT)
+
+
+def test_read_pdc(pdc_dictionary):
+    assert 'aliquot.yaml' in pdc_dictionary.schema
+    assert 'studyRunMetadata.yaml' not in pdc_dictionary.schema
