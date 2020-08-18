5b1ca99feb193a52924b84344aebafec02c204f3
jiaola <djiao@jhu.edu>
2020-08-18 10:01:58 -0400

    find the token and secret file

--- ccdh/gdrive/authorize.py
+++ ccdh/gdrive/authorize.py
@@ -1,5 +1,5 @@
 import pickle
-import os.path
+from pathlib import Path
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
@@ -7,23 +7,26 @@ from google.auth.transport.requests import Request
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly',
           'https://www.googleapis.com/auth/drive.metadata.readonly']
 
+PICKLE_PATH = Path(__file__).parent.parent.joinpath('token.pickle')
+CREDENTIALS_PATH = Path(__file__).parent.parent.joinpath('google_api_credentials.json')
+
 
 def authorize():
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if PICKLE_PATH.exists():
+        with open(PICKLE_PATH, 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('google_api_credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_PATH, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open(PICKLE_PATH, 'wb') as token:
             pickle.dump(creds, token)
     return creds
