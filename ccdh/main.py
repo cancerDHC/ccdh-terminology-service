29536d70ac681a2315aaa0451b076ea86d31e554
jiaola <djiao@jhu.edu>
2020-08-18 10:31:29 -0400

    Added header in tsv

--- ccdh/main.py
+++ ccdh/main.py
@@ -4,8 +4,9 @@ import csv
 
 def main():
     rows = icdc_values()
-    with open('icdc.tsv', 'w', newline='') as f_output:
+    with open('valuesets_2020-08-16.tsv', 'w', newline='') as f_output:
         tsv_output = csv.writer(f_output, delimiter='\t')
+        tsv_output.writerow(['CDM.Entity', 'CDM Attribute', 'Source', 'Source Entity', 'Source Attribute', 'Value'])
         for row in rows:
             tsv_output.writerow(row)
 
