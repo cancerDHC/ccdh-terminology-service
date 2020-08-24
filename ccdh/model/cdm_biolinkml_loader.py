1581bd02d4bc8cea544dcccad6aa56fe94869e34
jiaola <djiao@jhu.edu>
2020-08-24 13:39:08 -0400

    Fixed the loader to generate a working-version of specimen yaml

--- ccdh/model/cdm_biolinkml_loader.py
+++ ccdh/model/cdm_biolinkml_loader.py
@@ -1,6 +1,7 @@
-from biolinkml.meta import SchemaDefinition, SlotDefinition, ElementName, ClassDefinition
+from biolinkml.meta import SchemaDefinition, SlotDefinition, ElementName, ClassDefinition, ClassDefinitionName
 from typing import List
 from ccdh.cdm import class_definition
+import re
 
 from biolinkml.utils.yamlutils import extended_str, as_yaml
 
@@ -11,17 +12,30 @@ class TabularSchemaDefinitionLoader(object):
 
     @classmethod
     def load(cls, name, rows: List[List]) -> SchemaDefinition:
-        schema: SchemaDefinition = SchemaDefinition(name=name, id='https://ccdh.org/model/specimen')
-        klass: ClassDefinition = ClassDefinition(name=name)
+        schema: SchemaDefinition = SchemaDefinition(name=name, id='https://ccdh.org/model/specimen', title=name)
+        klass: ClassDefinition = ClassDefinition(name=name, is_a=ClassDefinitionName('Entity'))
         schema.classes = {name: klass}
         schema.license = 'https://creativecommons.org/publicdomain/zero/1.0/'
+        schema.prefixes = {
+            'biolinkml': 'https://w3id.org/biolink/biolinkml/',
+            'specimen': 'https://ccdh.org/specimen/'
+        }
+        schema.default_prefix = 'specimen'
+        schema.imports = ['prefixes', 'entities', 'datatypes']
         schema.description = rows[1][5]
-        schema.notes.append(rows[3][5])
+        schema.comments = rows[3][5].split('\n')
+        schema.notes.append('derived from [CDM_Dictionary_v1](https://docs.google.com/spreadsheets/d/1oWS7cao-fgz2MKWtyr8h2dEL9unX__0bJrWKv6mQmM4/)')
         schema.mappings.extend([i.strip() for i in rows[2][5].split('\n')])
+        deprecated = False
         for row in rows[5:]:
+            # todo: use the deprecated slot
+            # for exploratory attrs use subset
             if row[0] == 'DEPRECATED/EXPLORATORY ATTRIBUTES':
-                break
+                deprecated = True
+                continue
             slot: SlotDefinition = TabularSchemaDefinitionLoader.load_slot(row)
+            if deprecated:
+                slot.deprecated = True
             klass.slot_usage[slot.name] = slot
         return schema
 
@@ -29,11 +43,16 @@ class TabularSchemaDefinitionLoader(object):
     def load_slot(cls, row: List) -> SlotDefinition:
         slot: SlotDefinition = SlotDefinition(name=row[5])
         slot.description = row[6]
-        slot.mappings = []
-        slot.mappings.extend(['ADM:' + i.strip() for i in row[11].split('\n') if i.strip()])
-        slot.mappings.extend([i.strip().replace('.', ':', 1) for i in row[12].split('\n')])
-        slot.mappings.extend(['FHIR:' + i.strip() for i in row[13].split('\n')])
-        slot.range = ElementName(row[7])
+        mappings = []
+        mappings.extend(['ADM:' + i.strip() for i in row[11].split('\n') if i.strip()])
+        mappings.extend([i.strip().replace('.', ':', 1) for i in row[12].split('\n') if i.strip()])
+        mappings.extend(['FHIR:' + i.strip() for i in row[13].split('\n') if i.strip()])
+        slot.mappings = list(filter(lambda x: re.match(r'^\w+:[\w\-\_]+\.[\w\-\_]+$', x), mappings))
+        range_str = ElementName(row[7]).replace('*', '')
+        range_str = re.sub(r'\([^)]*\)', '', range_str).strip()
+        if '|' in range_str:
+            range_str = 'Or'.join([i.strip() for i in range_str.split('|')])
+        slot.range = range_str
         slot = TabularSchemaDefinitionLoader.cardinality_to_slot(slot, row[8])
         slot.comments.extend(filter(lambda a: len(a.strip()) > 0, row[9].split('\n')))
         return slot
@@ -46,14 +65,17 @@ class TabularSchemaDefinitionLoader(object):
         elif card == '0..m':
             slot.required = False
             slot.multivalued = True
+        if slot.name == 'id':
+            slot.required = True
         return slot
 
 
-def load_ccdh_specimen():
+def load_ccdh_specimen() -> str:
     rows: List[List] = class_definition('1oWS7cao-fgz2MKWtyr8h2dEL9unX__0bJrWKv6mQmM4', 'Specimen')
     schema: SchemaDefinition = TabularSchemaDefinitionLoader.load('Specimen', rows)
-    print(as_yaml(schema))
+    yaml = as_yaml(schema)
+    return '\n'.join([i for i in yaml.split('\n') if not re.match(r'^\s+name:.*', i)])
 
 
 if __name__ == '__main__':
-    load_ccdh_specimen()
+    print(load_ccdh_specimen())
