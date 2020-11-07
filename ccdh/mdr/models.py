893a518900fcfd8982cd8ad8501c9b623a50b3af
jiaola <djiao@jhu.edu>
2020-11-07 07:18:12 -0500

    Use dataclass decorator for models

--- ccdh/mdr/models.py
+++ ccdh/mdr/models.py
@@ -1,67 +1,78 @@
+from dataclasses import dataclass
 from py2neo.ogm import Model, Property, RelatedFrom, RelatedTo
 
 
+@dataclass
 class DataElement(Model):
     __primarykey__ = 'identifier'
-    identifier = Property()
-    title = Property()
-    context = Property()
-    entity = Property()
-    attribute = Property()
+    identifier: str = Property()
+    title: str = Property()
+    context: str = Property()
+    entity: str = Property()
+    attribute: str = Property()
 
     value_domain = RelatedFrom('ValueDomain', 'DOMAIN_OF')
     data_element_concept = RelatedTo('DataElementConcept', 'REPRESENTS')
 
 
+@dataclass
 class ValueDomain(Model):
     __primarykey__ = 'identifier'
-    identifier = Property()
+    identifier: str = Property()
     data_element = RelatedTo(DataElement)
 
 
+@dataclass
 class EnumeratedValueDomain(ValueDomain):
     __primarykey__ = 'identifier'
-    identifier = Property()
+    identifier: str = Property()
     permissible_values = RelatedFrom('PermissibleValue', 'PART_OF')
 
 
+@dataclass
 class PermissibleValue(Model):
     __primarykey__ = 'identifier'
-    identifier = Property()
+    __primarylabel__ = 'value'
+    identifier: str = Property()
+    value: str = Property()
     value_domain = RelatedTo(EnumeratedValueDomain)
     value_meaning = RelatedTo('ValueMeaning', 'MAPS_TO')
 
 
+@dataclass
 class ValueMeaning(Model):
     __primarykey__ = 'identifier'
-    identifier = Property()
-    code = Property()
-    code_system = Property()
-    label = Property()
+    identifier: str = Property()
+    code: str = Property()
+    code_system: str = Property()
+    display: str = Property()
 
-    permissible_value = RelatedFrom(PermissibleValue)
+    permissible_values = RelatedFrom(PermissibleValue)
     concept_domain = RelatedTo('ConceptualDomain', 'PART_OF')
 
 
+@dataclass
 class ConceptualDomain(Model):
     __primarykey__ = 'identifier'
-    identifier = Property()
-    name = Property()
+    identifier: str = Property()
+    name: str = Property()
     data_element_concept = RelatedTo('DataElementConcept', 'DOMAIN_OF')
 
 
+@dataclass
 class EnumeratedConceptualDomain(ConceptualDomain):
     __primarykey__ = 'identifier'
-    name = Property()
+    name: str = Property()
     value_meanings = RelatedFrom(ValueMeaning)
-    uri = Property()
+    uri: str = Property()
 
 
+@dataclass
 class DataElementConcept(Model):
     __primarykey__ = 'identifier'
-    name = Property()
-    object_class = Property()
-    property = Property()
+    name: str = Property()
+    object_class: str = Property()
+    property: str = Property()
     sconceptual_domain = RelatedFrom(ConceptualDomain)
     data_element = RelatedFrom(DataElement, 'REPRESENTS')
 
--- tests/ccdh/mdr/test_models.py
+++ tests/ccdh/mdr/test_models.py
@@ -1,7 +1,7 @@
 import pytest
 from py2neo import NodeMatcher
 
-from ccdh.mdr.models import DataElement, ValueDomain, EnumeratedValueDomain
+from ccdh.mdr.models import *
 
 
 @pytest.fixture
@@ -38,3 +38,14 @@ def value_domain(neo4j_graph):
 def test_enumerated_value_domain(neo4j_graph, value_domain):
     node = NodeMatcher(neo4j_graph).match('EnumeratedValueDomain').first()
     assert node is not None
+
+
+def test_value_meaning_dataclass():
+    vm = ValueMeaning(identifier='vm_1', code='12345', code_system='ncit', display='test')
+    pm = PermissibleValue(identifier='pm_1', value='my test')
+    vm.permissible_values.add(pm)
+    assert vm.display == 'test'
+    assert vm.code == '12345'
+    assert len(vm.permissible_values) == 1
+
+
