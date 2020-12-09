4d50c91737e551e25e8e8514125aeba5bceb04fb
jiaola <djiao@jhu.edu>
2020-12-08 20:56:10 -0500

    Remove flask implementation

--- ccdh/apis/db.py
+++
@@ -1,9 +0,0 @@
-from flask import current_app, g
-from py2neo import Graph
-from ccdh.config import neo4j_graph
-
-
-def get_neo4j_graph() -> Graph:
-    if 'neo4j_graph' not in g:
-        g.neo4j_graph = neo4j_graph()
-    return g.neo4j_graph
