18983076a6b3f6fb3b97f9ccd81d863954b97b73
jiaola <djiao@jhu.edu>
2021-05-03 14:31:46 -0400

    Forward root to docs

--- ccdh/api/app.py
+++ ccdh/api/app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from ccdh.api.routers import data_element_concepts, data_elements, mappings, value_meanings, permissible_values
 
@@ -9,3 +11,15 @@ app.include_router(data_elements.router)
 app.include_router(permissible_values.router)
 app.include_router(value_meanings.router)
 app.include_router(mappings.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+
+@app.get("/")
+def root():
+    return RedirectResponse(url="/docs")
