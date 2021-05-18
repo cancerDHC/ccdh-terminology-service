f35ee5290f690e6eed224543c5d48f4747746ddf
jiaola <djiao@jhu.edu>
2021-05-18 13:06:34 -0400

    Fixed retrieving harmonized attribute value mappings

--- ccdh/api/routers/mappings.py
+++ ccdh/api/routers/mappings.py
@@ -56,7 +56,7 @@ router = APIRouter(
                     "description": "Return the JSON mapping set or a TSV file.",
                 }
             })
-async def get_data_element_mapping(system: str, entity: str, attribute: str, request: Request) -> MappingSet:
+async def get_node_attribute_value_mapping(system: str, entity: str, attribute: str, request: Request) -> MappingSet:
     mapping_set = mdr_graph.find_mappings_of_node_attribute(system, entity, attribute, pagination=False)
     if request.headers['accept'] == 'text/tab-separated-values+sssom':
         return StreamingResponse(generate_sssom_tsv(MappingSet.parse_obj(mapping_set.__dict__)), media_type='text/tab-separated-values+sssom')
@@ -64,7 +64,7 @@ async def get_data_element_mapping(system: str, entity: str, attribute: str, req
         return mapping_set.__dict__
 
 
-@router.get('/crdc-h/{context}/{entity}/{attribute}', response_model=MappingSet,
+@router.get('/crdc-h/{system}/{entity}/{attribute}', response_model=MappingSet,
             responses={
                 200: {
                     "content": {
@@ -73,9 +73,8 @@ async def get_data_element_mapping(system: str, entity: str, attribute: str, req
                     "description": "Return the JSON mapping set or a TSV file.",
                 }
             })
-async def get_data_element_concept_mapping(system: str, entity: str, attribute: str, request: Request) -> MappingSet:
+async def get_harmonized_attribute_value_mapping(system: str, entity: str, attribute: str, request: Request) -> MappingSet:
     mapping_set = mdr_graph.find_mappings_of_harmonized_attribute(system, entity, attribute, pagination=False)
-    mapping_set.mappings = list(map(map_mapping, mapping_set.mappings))
     if request.headers['accept'] == 'text/tab-separated-values+sssom':
         return StreamingResponse(generate_sssom_tsv(MappingSet.parse_obj(mapping_set.__dict__)), media_type='text/tab-separated-values+sssom')
     else:
