1e4a35174d11b4c3d666cda44107e0dff610f8cf
jiaola <djiao@jhu.edu>
2021-05-25 23:37:03 -0400

    Add extension to tccm concept reference endpoint

---
+++ ccdh/api/routers/ccdh_concept_references.py
@@ -0,0 +1,20 @@
+from fastapi import Request
+from starlette.responses import StreamingResponse
+from tccm_api.routers import concept_reference
+
+from ccdh.api.routers.mappings import generate_sssom_tsv
+from ccdh.api.routers.models import MappingSet
+from ccdh.config import neo4j_graph
+from ccdh.db.mdr_graph import MdrGraph
+
+router = concept_reference.router
+mdr_graph = MdrGraph(neo4j_graph())
+
+
+@router.get('/{curie}/mappings', response_model=MappingSet)
+def get_concept_reference_mappings(curie: str, request: Request):
+    mapping_set = mdr_graph.find_mappings_of_concept_reference(curie)
+    if request.headers['accept'] == 'text/tab-separated-values+sssom':
+        return StreamingResponse(generate_sssom_tsv(MappingSet.parse_obj(mapping_set.__dict__)), media_type='text/tab-separated-values+sssom')
+    else:
+        return mapping_set.__dict__
