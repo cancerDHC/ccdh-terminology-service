01250b4f8412d387f64e5c1113851aee87ccb34f
jiaola <djiao@jhu.edu>
2021-05-25 23:38:04 -0400

    updated router imports

--- ccdh/api/app.py
+++ ccdh/api/app.py
@@ -4,18 +4,15 @@ from starlette.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from tccm_api.db.tccm_graph import TccmGraph
 
-from ccdh.api.routers import harmonized_attributes, node_attributes, mappings, permissible_values, enumerations, models
+from ccdh.api.routers import mappings, permissible_values, enumerations, models, ccdh_concept_references
 from tccm_api.routers import concept_reference
 
 app = FastAPI(title='CCDH Terminology Service API')
 
 app.include_router(models.router)
-app.include_router(harmonized_attributes.router)
-app.include_router(node_attributes.router)
 app.include_router(enumerations.router)
 app.include_router(permissible_values.router)
-app.include_router(mappings.router)
-app.include_router(concept_reference.router)
+app.include_router(ccdh_concept_references.router)
 
 app.add_middleware(
     CORSMiddleware,
