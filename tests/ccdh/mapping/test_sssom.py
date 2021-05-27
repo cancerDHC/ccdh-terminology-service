49dbd4940b83bbe180b4f3208db7e031bf54f499
jiaola <djiao@jhu.edu>
2021-05-27 14:43:40 -0400

    mapping test

--- tests/ccdh/mapping/test_sssom.py
+++ tests/ccdh/mapping/test_sssom.py
@@ -1,7 +1,10 @@
-from sssom.sssom_datamodel import *
+from sssom.sssom_datamodel import Mapping, MappingSet
 from sssom.io import *
 import pytest
+from ccdh.api.routers.mappings import generate_sssom_tsv
+from ccdh.namespaces import NAMESPACES
 # Some tests to demonstrate how to use sssom-py
+
 
 @pytest.fixture
 def curie_map() -> dict:
@@ -10,12 +13,13 @@ def curie_map() -> dict:
     }
 
 
-def test_mapping_set(curie_map):
+def test_mapping_set():
+    curie_map = NAMESPACES
     mapping_set = MappingSet()
     mapping = Mapping()
     mapping.subject_id = 'EX:s1'
     mapping.object_id = 'EX:o1'
     mapping.comment = 'a mapping example'
     mapping.mapping_provider = 'ccdh'
-    mapping_set_doc = MappingSetDocument(mapping_set, curie_map)
-    print(to_tsv)
+    mapping_set.curie_map = curie_map
+    mapping_set.mappings.append(mapping)
